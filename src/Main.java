da2bada7e96f3e2f0585ac0092ff36614cf81583
FORZASPYKE <badillafimbresluis@gmail.com>
2024-08-28 11:07:58 -0700

    Se cambia la lectura por Scanner a lectura de argumentos desde la linea de comandos

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Escribe una palabra:");
-        String Palabra = scanner.nextLine();
+        //Se valida si se introdujo un argumento
+        if(args.length <= 0){
+            System.out.println("No se introdujo ningun argumento");
+        }else {
+            //se le asigna el argumento a la cadena Argumento
+            String Argumento = args[0];
+            System.out.println("La cadena es:" + Argumento);
 
-        // Las expresiones regulares
-        String HolaMundo = "(?i)^Hola mundo$";
-        String PalabrasVarias = "\\b(?:Java|Python|Go|Pascal|Perl)\\b";
-        String unisoncorreo = "^[a-zA-Z0-9._%+-]+@(unison\\.mx|uson\\.mx)$";
-        String nombrearchivo = "^ISI\\d{4}-[12]\\.(?:txt|csv)$";
 
-        // comparacion de palabras
-        System.out.println("¿Es una expresión válida?");
-        System.out.println("Hola mundo: " + Pattern.matches(HolaMundo, Palabra));
-        System.out.println("Palabras: " + Pattern.matches(PalabrasVarias, Palabra));
-        System.out.println("Correo UNISON: " + Pattern.matches(unisoncorreo, Palabra));
-        System.out.println("Nombre de archivo: " + Pattern.matches(nombrearchivo, Palabra));
+            // Las expresiones regulares
+            String HolaMundo = "(?i)^Hola mundo$";
+            String PalabrasVarias = "\\b(?:Java|Python|Go|Pascal|Perl)\\b";
+            String unisoncorreo = "^[a-zA-Z0-9._%+-]+@(unison\\.mx|uson\\.mx)$";
+            String nombrearchivo = "^ISI\\d{4}-[12]\\.(?:txt|csv)$";
 
-        scanner.close();
+            // comparacion de palabras
+            System.out.println("¿Es una expresión válida?");
+            System.out.println("Hola mundo: " + Pattern.matches(HolaMundo, Argumento));
+            System.out.println("Palabras: " + Pattern.matches(PalabrasVarias, Argumento));
+            System.out.println("Correo UNISON: " + Pattern.matches(unisoncorreo, Argumento));
+            System.out.println("Nombre de archivo: " + Pattern.matches(nombrearchivo, Argumento));
+
+            scanner.close();
+        }
+
     }
 }
